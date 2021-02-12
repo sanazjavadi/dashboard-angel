1550c5887b1890e912a6c76760af78ef89ceaed7
sanaz_javadi <sanazjavadi72@yahoo.com>
2021-02-12 19:52:09 +0330

    implement package's

---
+++ src/layouts/sidebar/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//styles
+import styles from './styles/sidebar.module.scss'
+
+
+ function Sidebar(props) {
+    
+
+    return (
+      <div className={styles.sidebar}>
+      </div>
+    )
+}
+
+export default Sidebar;
